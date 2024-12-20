e52ae1e4ce1be0901c233d64cb3e28a60e1cbc8b
Dennis <denniswei305@gmail.com>
2024-12-20 17:56:47 +0800

    initial code

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
 from bs4 import BeautifulSoup
-
+from selenium import webdriver
 import requests
+import time
 
-html_text = requests.get('https://www.babymap.hk/explore/?sort=random').text
-# print(html_text)
+# load webpage to get results
+driver = webdriver.Chrome()
+driver.get('https://www.babymap.hk/explore')
+
+
+all_postings = []
+
+while True:
+
+    # grab html text from webpage
+    html_text = driver.page_source
+
+    time.sleep(5)
+
+    # find all postings
+    soup = BeautifulSoup(html_text, 'lxml')
+    centrePostings = soup.find_all('div', class_ = 'col-md-12 grid-item')
+
+    for post in centrePostings:
+        postName = post.find('h4', class_ = 'case27-primary-text listing-preview-title').text.replace('                ','')
+        postCatagories = post.find_all('span', class_ = 'category-name')
+
+        print(postName)
+        print(postCatagories[1].text)
+
+        NameE
+        
+
+    # find next page element
+    nextButton = soup.find('a', rel = 'next')
+
+    if not nextButton:
+        break
+
+    driver.get(nextButton['href'])
+
+    time.sleep(5)
+    break
+
+    
+
+# print(centrePostings[0])
 
 with open('page_content.txt', 'w', encoding='utf-8') as f:
-    f.write(html_text)
-
-soup = BeautifulSoup(html_text, 'lxml')
-centrePostings = soup.find_all('div', class_ = 'col-md-12 grid-item')
-
-print(centrePostings)
+    for text in centrePostings:
+        f.write(str(text))
 
 # with open('home.html')
